1cf7bb6b3300f5e921059adc6687f7674e695a4f
durongzheng <drz@easyman.com.cn>
2025-03-28 14:49:53 +0800

    fix bug.

--- tests/test_conn.py
+++ tests/test_conn.py
@@ -5,12 +5,11 @@ from config import get_config
 cfg = get_config()
 
 def test_redis_connection():
-    pool = redis.ConnectionPool(
+    r = redis.Redis(
         host=cfg.REDIS_HOST,
         port=cfg.REDIS_PORT,
         db=cfg.REDIS_DB,
-        password=cfg.REDIS_PASSWORD,
-        decode_responses = True
+        password=cfg.REDIS_PASSWORD,  
+        decode_responses=True
     )
-    r = redis.Redis(connection_pool=pool)
     assert r.ping()

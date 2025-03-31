7f053edd310052ced6dec7a06b6ffb663979c290
durongzheng <drz@easyman.com.cn>
2025-03-31 17:55:06 +0800

    fix bug

--- bot/config/__init__.py
+++ bot/config/__init__.py
@@ -1,4 +1,4 @@
-from .config import get_config, Settings
+from .config import get_config, Settings, BASE_DIR
 from .celery import celery_config
 
-__all__ = ["get_config", "Settings", "celery_config"]
+__all__ = ["get_config", "Settings", "BASE_DIR", "celery_config"]

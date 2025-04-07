ab796fbb444b0455b5f31614bda48c69824c9b71
durongzheng <drz@easyman.com.cn>
2025-04-07 11:25:28 +0800

    增加语音识别处理

--- bot/core/asr.py
+++ bot/core/asr.py
@@ -14,7 +14,9 @@ class AutoSpeechRecognition:
         self.asr = pipeline(
             task=Tasks.auto_speech_recognition,
             model=model_name,
-            model_revision="v2.0.4")
+            model_revision="v2.0.4",
+            preprocessor=None,
+            device=self.device)
         
     def speech_recognition(self, 
             input_path: str) -> list:
@@ -26,6 +28,6 @@ if __name__ == '__main__':
     asr_model = asr_model if os.path.exists(asr_model) else 'iic/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-pytorch'
     asr = AutoSpeechRecognition(model_name=asr_model)
     
-    result = asr.speech_recognition(input_path=os.path.join(DENOISED_ROOT_PATH, '20250406094822/0000000000_0000407680.wav'))
+    result = asr.speech_recognition(input_path=os.path.join(DENOISED_ROOT_PATH, '20250407110552/0000000000_0000407680.wav'))
     print(type(result), result)
 

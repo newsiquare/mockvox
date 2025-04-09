39d1da1e2cb30f150b0ab00aeca14f8001e41f22
durongzheng <drz@easyman.com.cn>
2025-04-09 11:56:05 +0800

    lfs调不通，去掉

--- src/bot/text/chinese.py
+++ src/bot/text/chinese.py
@@ -1,5 +1,6 @@
 import os
 import cn2an
+from pypinyin import G2PWPinyin
 
 normalizer = lambda x: cn2an.transform(x, "an2cn")
 
@@ -7,3 +8,5 @@ pinyin_to_symbol_map = {
     line.split("\t")[0]: line.strip().split("\t")[1]
     for line in open(os.path.join(os.path.dirname(__file__), "opencpop-strict.txt")).readlines()
 }
+
+g2pw = G2PWPinyin()

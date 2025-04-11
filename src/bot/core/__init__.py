7e443fe81dfbca5a85ab4f075aa3b094299625e1
durongzheng <drz@easyman.com.cn>
2025-04-11 19:17:23 +0800

    text2semantic: SynthesizerTrn

--- src/bot/core/__init__.py
+++ src/bot/core/__init__.py
@@ -2,11 +2,13 @@ from .slicer import Slicer, load_audio
 from .denoiser import AudioDenoiser
 from .asr import AutoSpeechRecognition
 from .data_process import DataProcessor
+from .feature_extract import FeatureExtractor
 
 __all__ = [
     "Slicer", 
     "load_audio",
     "AudioDenoiser",
     "AutoSpeechRecognition",
-    "DataProcessor"
+    "DataProcessor",
+    "FeatureExtractor"
 ]
--- src/bot/models/__init__.py
+++ src/bot/models/__init__.py
@@ -1,8 +1,82 @@
 # from .SpeechSeparation import BSRoformer, MelBandRoformer
-from .HuBERT import CNHubert
+from .HuBERT import *
+from .tools import *
+from .BaseModules import *
+from .quantize import *
+from .attentions import *
+from .mrte import MRTE, MELEncoder, SpeakerEncoder
 
 __all__ = [
     # "BSRoformer", 
     # "MelBandRoformer",
-    "CNHubert"
+    "CNHubert",
+
+    # quantize
+    "ResidualVectorQuantizer",
+
+    # 工具集函数
+    "init_weights",
+    "get_padding",
+    "intersperse",
+    "kl_divergence",
+    "rand_gumbel",
+    "rand_gumbel_like",
+    "slice_segments",
+    "rand_slice_segments",
+    "get_timing_signal_1d",
+    "add_timing_signal_1d",
+    "cat_timing_signal_1d",
+    "subsequent_mask",
+    "fused_add_tanh_sigmoid_multiply",
+    "convert_pad_shape",
+    "shift_1d",
+    "sequence_mask",
+    "generate_path",
+    "clip_grad_value_",
+    "squeeze",
+    "unsqueeze",
+
+    # 标准化流模型（Normalizing Flows）的关键组件
+    "rational_quadratic_spline",
+    "unconstrained_rational_quadratic_spline",
+    "searchsorted",
+    "piecewise_rational_quadratic_transform",
+
+    # Base Modules
+    "LayerNorm",
+    "ConvReluNorm",
+    "DDSConv",
+    "WaveNet",    
+    "ResBlock1",
+    "ResBlock2",    
+    "MultiHeadAttention",
+    "ScaledDotProductAttention",    
+    "MelStyleEncoder",
+    "MelStyleEncoderVAE",    
+    "ConvFlow",
+    "ResidualCouplingLayer",
+    "ActNorm",
+    "InvConvNear",
+    "Log",
+    "Flip",
+    "ElementwiseAffine",    
+    "Conv1dGLU",
+    "ConvNorm",
+    "LinearNorm",
+    "Mish",
+
+    # 注意力
+    "Encoder",
+    "Decoder"
+    "MultiHeadAttention"
+    "FFN",
+    "FFT",
+    "Depthwise_Separable_Conv1D",
+    "Depthwise_Separable_TransposeConv1D",
+    "TransformerCouplingLayer",
+
+    # MRTE
+    "MRTE",
+    "MELEncoder",
+    "SpeakerEncoder"
 ]
